0dff951be39dad7d3a1db97b7a1428f2475191a4
Handy-caT <maksim_volkov03@mail.ru>
2021-09-19 21:53:12 +0300

    Initial Commit

---
+++ Gauss/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Gauss
+//
+//  Created by Максим on 19.09.2021.
+//  Copyright © 2021 Максим. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
