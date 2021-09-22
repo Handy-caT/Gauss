6b1dcd875aa666698081c0ee08f067e8aceaf724
Handy-caT <37216852+Handy-caT@users.noreply.github.com>
2021-09-23 00:15:01 +0300

    Update main.cpp
    half of StraightGauss made

--- Gauss/main.cpp
+++ Gauss/main.cpp
@@ -18,22 +18,72 @@ int Random(int min, int max) {
     return rand() % (max- min + 1) + min;
 }
 
+double** CopyMatrix(double** Matrix, int rows, int cols) {
+    
+    int i = 0;
+    int j = 0;
+    
+    double** result = new double*[cols];
+    
+    while( i < cols ) {
+        
+        result[i] =  new double[rows];
+        j = 0;
+        
+        while(j < rows) {
+            
+            result[i][j] = Matrix[i][j];
+            j++;
+            
+        }
+        
+        i++;
+    }
+    
+    return result;
+    
+}
+
+double** MatrixMake(int rows, int cols) {
+    
+    int i = 0;
+    int j = 0;
+    
+    double** result = new double*[cols];
+    
+    while( i < cols ) {
+        
+        result[i] =  new double[rows];
+        j = 0;
+        
+        while(j < rows) {
+            
+            result[i][j] = 0;
+            j++;
+            
+        }
+        
+        i++;
+    }
+    
+    return result;
+}
+
 double** RandomMatrix(int rows, int cols) {
     
     int i = 0;
     int j = 0;
     
-    double** result = new double*[rows]; //RESULT i/j
+    double** result = new double*[cols]; //RESULT i/j
     
-    while( i < rows) {
+    while( i < cols) {
         
-        result[i] = new double[cols];
+        result[i] = new double[rows];
         j = 0;
         
-        while( j < cols) {
+        while( j < rows) {
             
-            result[i][j] = Random(-100, 100);
-            
+            result[i][j] = (double) Random(-100, 100);
             j++;
         }
         
@@ -98,40 +148,62 @@ double** MatrixMultiply(double** left, double** right, int row1, int p, int col2
     
 }
 
-double** MatrixMultiply(double** left, double** right, int row1, int p, int col2) {
+double** RowDivide(double** A, int row, int cols, int firstCol, double number) {
     
-    //LEFT row1/p
-    //RIGHT p/col2
+    int i = firstCol;
+    while( i < cols ) {
+        A[row][i] = A[row][i]/number;
+        //std::cout << A[row][i] << " " << A[row][i]/number << "\n";
+        i++;
+    }
+    
+    return A;
+}
+
+double** ZeroCol(double** A, int col, int rows) {
+    
+    int i = 0;
+    while( i < rows ) {
+        A[i][col] = (double) 0;
+        i++;
+    }
+    
+    return A;
+}
+
+double** StraightGauss(double** userA, int rows, int cols, double** userf, int fCols) {
+    
     int i = 0;
     int j = 0;
-    int k = 0;
     
-    double** result = new double*[row1]; //RESULT i/j
+    double leading;
     
-    while( i < row1) {
+    double** X = MatrixMake(rows, fCols);
+    double** A = CopyMatrix(userA, rows, cols);
+    double** f = CopyMatrix(userf, rows, fCols);
+    
+    MatrixShow(A, 3, 3);
+    
+    while( i < rows - 1 ) {
         
-        result[i] = new double[col2];
-        j = 0;
+        leading = A[i][i];
         
-        while( j < col2) {
-            
-            result[i][j] = 0;
-            k = 0;
-            
-            while (k < p) {
-                
-                result[i][j] += left[row1][k] * right[k][col2];
-                k++;
-            }
-            
-            j++;
-        }
+        ZeroCol(A, i, rows);
+        MatrixShow(A, 3, 3);
+        RowDivide(A, i, cols, i+1, leading);
+        MatrixShow(A, 3, 3);
+        RowDivide(f, i, fCols, i, leading);
+        
+        A[i][i] = 1;
         
         i++;
     }
     
-    return result;
+    leading = A[i][i];
+    RowDivide(f, i, fCols, i, leading);
+    A[i][i] = 1;
     
+    return A;
 }
 
 int main(int argc, const char * argv[]) {
@@ -148,7 +220,7 @@ int main(int argc, const char * argv[]) {
     a[1][0] = 2;
     a[1][1] = 3;
     
-    MatrixShow(a, 2, 2);*/
+    MatrixShow(a, 2, 2);
     
     double** a;
     double** b;
@@ -163,9 +235,19 @@ int main(int argc, const char * argv[]) {
     
     c = MatrixMultiply(a, b, 3, 3, 3);
     
+    MatrixShow(c, 3, 3);*/
+    
+    double** a;
+    double** b;
+    double** c;
+    
+    RandomSet();
+    a = RandomMatrix(3, 3);
+    b = RandomMatrix(3, 1);
+    
+    c = StraightGauss(a, 3, 3, b, 1);
+    
     MatrixShow(c, 3, 3);
-    
-    
     
     // insert code here...
     //std::cout << "Hello, World!\n";
