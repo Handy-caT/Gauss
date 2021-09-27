28827aa0e9056cc3b525d3b853c20f7234931f14
Handy-caT <37216852+Handy-caT@users.noreply.github.com>
2021-09-27 14:12:34 +0300

    Final commit

--- Gauss/main.cpp
+++ Gauss/main.cpp
@@ -7,6 +7,7 @@
 //
 
 #include <iostream>
+#include <iomanip>
 #include <cstdlib>
 #include <ctime>
 #include <cmath>
@@ -18,106 +19,89 @@ void RandomSet() {
 }
 
 int Random(int min, int max) {
-    return rand() % (max- min + 1) + min;
+    return rand() % (max - min + 1) + min;
+}
+
+bool doubleEqualsZero(double number) {
+    return  std::abs(number) < std::numeric_limits<double>::epsilon()*10;
+
 }
 
 double** CopyMatrix(double** Matrix, int rows, int cols) {
-    
     int i = 0;
     int j = 0;
     
-    double** result = new double*[cols];
+    double** result = new double*[rows];
     
     while( i < rows ) {
-        
-        result[i] =  new double[rows];
+        result[i] =  new double[cols];
         j = 0;
         
         while(j < cols) {
-            
             result[i][j] = Matrix[i][j];
             j++;
             
         }
-        
         i++;
     }
-    
     return result;
-    
 }
 
 double** MatrixMake(int rows, int cols, bool unitary) {
-    
     int i = 0;
     int j = 0;
     
-    double** result = new double*[cols];
+    double** result = new double*[rows];
     
     while( i < rows ) {
-        
-        result[i] =  new double[rows];
+        result[i] =  new double[cols];
         j = 0;
         
         while(j < cols) {
-            
             if(unitary && i == j) result[i][j] = 1;
             else result[i][j] = 0;
 
             j++;
-            
         }
-        
         i++;
     }
-    
     return result;
 }
 
 double** RandomMatrix(int rows, int cols) {
-    
     int i = 0;
     int j = 0;
     
-    double** result = new double*[cols]; //RESULT i/j
+    double** result = new double*[rows]; //RESULT i/j
     
     while( i < rows) {
-        
-        result[i] = new double[rows];
+        result[i] = new double[cols];
         j = 0;
         
         while( j < cols) {
-            
-            result[i][j] = (double) Random(-100, 100); //наоборот тк заполнение по строкам
+            result[i][j] = (double) Random(-100, 100);
             j++;
         }
-        
         i++;
     }
-    
     return result;
 }
 
 void InputMatrix(double** matrix, int rows, int cols) {
-    
     int i = 0;
     int j = 0;
     
     while( i < rows ) {
-        
         j = 0;
         while( j < cols) {
             std::cin >> matrix[i][j];
             j++;
         }
-        
         i++;
     }
-    
 }
 
 void MatrixShow(double** matrix, int rows, int cols) {
-    
     int i = 0;
     int j = 0;
     
@@ -125,25 +109,21 @@ void MatrixShow(double** matrix, int rows, int cols) {
         
         j = 0;
         while( j < cols) {
-            std::cout << matrix[i][j] << " ";
+            std::cout <<  std::setw(12) << matrix[i][j] << " ";
             j++;
         }
-        
         std::cout << "\n";
         i++;
     }
-    
 }
 
 double** MatrixResidual(double** left, double** right, int rows, int cols) {
-    
     double** result = MatrixMake(rows, cols, false);
     
     int i = 0;
     int j = 0;
     
     while( i < rows) {
-        
         j = 0;
         while( j < cols) {
             result[i][j] = left[i][j] - right[i][j];
@@ -155,54 +135,41 @@ double** MatrixResidual(double** left, double** right, int rows, int cols) {
 }
 
 double** MatrixMultiply(double** left, double** right, int row1, int p, int col2) {
-    
-    //LEFT row1/p
-    //RIGHT p/col2
     int i = 0;
     int j = 0;
     int k = 0;
     
-    double** result = new double*[row1]; //RESULT i/j
+    double** result = new double*[row1];
     
     while( i < row1) {
-        
         result[i] = new double[col2];
         j = 0;
         
         while( j < col2) {
-            
             result[i][j] = 0;
             k = 0;
             
             while (k < p) {
-                
                 result[i][j] += left[i][k] * right[k][j];
                 k++;
             }
-            
             j++;
         }
-        
         i++;
     }
-    
     return result;
-    
 }
 
 double** RowDivide(double** A, int row, int cols, int firstCol, double number) {
-    
     int i = firstCol;
     while( i < cols ) {
         A[row][i] = A[row][i]/number;
         i++;
     }
-    
     return A;
 }
 
 double** RowsResidual(double** A, int firstRow, int rows, int cols, double** f, int fCols) {
-    
     int i = firstRow + 1;
     int j = 0;
     int k = 0;
@@ -210,13 +177,11 @@ double** RowsResidual(double** A, int firstRow, int rows, int cols, double** f, 
     int firstCol = firstRow;
     
     while( i < rows) {
-        
         j = firstRow + 1;
         k = 0;
         number = A[i][firstCol];
         
         while( j < cols || k < fCols) {
-            
             if( j < cols ) {
                 A[i][j] = A[i][j] - A[firstRow][j]*number;
                 j++;
@@ -225,27 +190,22 @@ double** RowsResidual(double** A, int firstRow, int rows, int cols, double** f, 
                 f[i][k] = f[i][k] - f[firstRow][k]*number;
                 k++;
             }
-            
         }
         i++;
     }
-    
     return A;
 }
 
 double** ZeroCol(double** A, int col, int rows) {
-    
     int i = col;
     while( i < rows ) {
         A[i][col] = (double) 0;
         i++;
     }
-    
     return A;
 }
 
 double** ReverseSteps(double** userA, int rows, int cols, double** userf, int fCols) {
-    
     int i = rows - 2;
     int j = 0;
     int k = 0;
@@ -255,87 +215,72 @@ double** ReverseSteps(double** userA, int rows, int cols, double** userf, int fC
     double** X = CopyMatrix(userf, rows, fCols);
     
     while( i >= 0) {
-        
         j = cols - 1;
         k = rows - 1;
         
-        
         while( j != i ){
-            
             a = 0;
             
-            while( a < fCols )
-            {
+            while( a < fCols ) {
                 X[i][a] = X[i][a] - userA[i][j]*X[k][a];
                 a++;
             }
-            
             k--;
             j--;
         }
-        
         i--;
     }
-    
     return X;
 }
 
 double** RowsChange(double** A, int cols, int from, int to, double** f, int fCols) {
-    
     int i = 0;
     
     while( i < cols || i < fCols) {
-        
         if( i < cols ) std::swap(A[from][i],A[to][i]);
         if( i < fCols ) std::swap(f[from][i],f[to][i]);
         
         i++;
-        
     }
-    
     return A;
-    
 }
 
 int MaxInCol(double** A, int col, int rows,int first){
-    
     double max = abs(A[first][col]);
     int imax = first;
     int i = first;
     
     while( i < rows) {
-        
         if( abs(A[i][col]) > max) {
             max = abs(A[i][col]);
             imax = i;
         }
-        
         i++;
     }
-    
     return imax;
 }
 
     
 double** ImprovedGauss(double** userA, int rows, int cols, double** userf, int fCols) {
-        
     int i = 0;
     int from = 0;
+    
+    bool error = false;
         
     double leading;
         
     double** X;
-    double** A = userA;//CopyMatrix(userA, rows, cols);
-    double** f = userf;//CopyMatrix(userf, rows, fCols);
+    double** A = CopyMatrix(userA, rows, cols);
+    double** f = CopyMatrix(userf, rows, fCols);
         
-    while( i < rows - 1 ) {
-            
+    while( i < rows - 1 && !error ) {
         from = MaxInCol(A, i, rows, i);
         if( from != i ) {
             RowsChange(A, cols, from, i, f, fCols);
         }
         leading = A[i][i];
-              
+        if(doubleEqualsZero(leading)) error = true;
+        else {
         RowDivide(A, i, cols, i+1, leading);
         RowDivide(f, i, fCols, 0, leading);
         RowsResidual(A, i, rows, cols, f, fCols);
@@ -343,21 +288,27 @@ double** ImprovedGauss(double** userA, int rows, int cols, double** userf, int f
         A[i][i] = 1;
 
         i++;
+        }
     }
+    
+    if(!error) {
         
-    leading = A[i][i];
-    RowDivide(f, i, fCols, 0, leading);
-    A[i][i] = 1;
+        leading = A[i][i];
+        if(doubleEqualsZero(leading)) X = nullptr;
+        else {
+            RowDivide(f, i, fCols, 0, leading);
+            A[i][i] = 1;
 
-    X = ReverseSteps(A, rows, cols, f, fCols);
-        
+            X = ReverseSteps(A, rows, cols, f, fCols);
+        }
+    }
+    else X = nullptr;
+    
     return X;
-        
 }
     
     
 double** StraightGauss(double** userA, int rows, int cols, double** userf, int fCols) {
-    
     int i = 0;
     
     double leading;
@@ -367,7 +318,6 @@ double** StraightGauss(double** userA, int rows, int cols, double** userf, int f
     double** f = CopyMatrix(userf, rows, fCols);
     
     while( i < rows - 1 ) {
-        
         leading = A[i][i];
         
         RowDivide(A, i, cols, i+1, leading);
@@ -389,7 +339,6 @@ double** StraightGauss(double** userA, int rows, int cols, double** userf, int f
 }
 
 double** ReverseMatrix(double** matrix, int rows, int cols) {
-    
     double** reverse;
     double** E = MatrixMake(rows, cols, true);
     reverse = ImprovedGauss(matrix, rows, cols, E, cols);
@@ -398,7 +347,6 @@ double** ReverseMatrix(double** matrix, int rows, int cols) {
 }
 
 double** MakeX(int rows) {
-    
     double** X = MatrixMake(rows, 1, false);
     int i = 0;
     
@@ -408,11 +356,9 @@ double** MakeX(int rows) {
     }
     
     return X;
-    
 }
 
 double** FirstTask() {
-    
     int iMax;
     double inaccuracy;
     
@@ -425,34 +371,43 @@ double** FirstTask() {
     
     double** X1 = ImprovedGauss(A, 10, 10, f, 1);
     
-    MatrixShow(A, 10, 10);
-    MatrixShow(f, 10, 1);
-    
-    MatrixShow(X, 10, 1);
-    MatrixShow(X1, 10, 1);
-    
-    double** m = MatrixResidual(X1, X, 10, 1);
-    
-    iMax = MaxInCol(m, 1, 10, 0);
-    inaccuracy = abs(m[iMax][0])/10.0;
-    std::cout << inaccuracy << std::endl << std::endl;
+    if( X1 != nullptr)
+    {
+        std::cout << "Matrix A:" << std::endl;
+        MatrixShow(A, 10, 10);
+        std::cout << "Matrix f:" << std::endl;
+        MatrixShow(f, 10, 1);
+        std::cout << "Matrix X:" << std::endl;
+        MatrixShow(X, 10, 1);
+        std::cout << "Matrix X from ImrovedGauss:" << std::endl;
+        MatrixShow(X1, 10, 1);
+        
+        double** m = MatrixResidual(X1, X, 10, 1);
+        
+        iMax = MaxInCol(m, 1, 10, 0);
+        inaccuracy = abs(m[iMax][0])/10.0;
+        std::cout << "Inaccuracy: " << inaccuracy << std::endl << std::endl;
+    }
+    else {
+        std::cout << "Solution of thy system can't be found"<< std::endl;
+        A = nullptr;
+    }
     
     return A;
 }
 
 void SecondTask(double** A) {
-    
     double** A1 = ReverseMatrix(A, 10, 10);
+    std::cout << "Reversed matrix A:" << std::endl;
     MatrixShow(A1, 10, 10);
     std::cout <<  std::endl;
     double** MaybeE = MatrixMultiply(A, A1, 10, 10, 10);
+    std::cout << "Matrix A*A1:" << std::endl;
     MatrixShow(MaybeE, 10, 10);
     std::cout <<  std::endl;
-    
 }
 
 void ThirdTask() {
-    
     int n = 2;
     int imax;
     double** A;
@@ -461,45 +416,35 @@ void ThirdTask() {
     double** X1;
     double** m;
     double inaccuracy;
+    
+    std::cout << std::setw(5) << "n:" << std::setw(12) << "Inaccuracy:" << std::endl;
         
     while(n < 103)
     {
         A = RandomMatrix(n , n);
-        //MatrixShow(A, n, n);
         X = MakeX(n);
-       // MatrixShow(X, n, 1);
         f = MatrixMultiply(A, X, n, n, 1);
-        //MatrixShow(f, n, 1);
         
         X1 = ImprovedGauss(A, n, n, f, 1);
         m = MatrixResidual(X1, X, n, 1);
         
         imax = MaxInCol(m, 1, n, 0);
         inaccuracy = abs(m[imax][0])/((double) (n));
-        std::cout << n << "  " << inaccuracy << std::endl;
+        std::cout << std::setw(5) << n << std::setw(12) << inaccuracy << std::endl;
         
         n = n + 10;
     }
-    
 }
 
 int main(int argc, const char * argv[]) {
     
+    std::cout << "Epsilon: " << std::numeric_limits<double>::epsilon()*10 << std::endl;
     double** A;
     
     RandomSet();
     A = FirstTask();
-    SecondTask(A);
+    if(A != nullptr) SecondTask(A);
     ThirdTask();
-    /*
-     
-     2  0
-     12  2.03541e-16
-     22  4.88296e-14
-     32  2.35229e-15
     
-     */
-    //A = MakeX(112);
-    //MatrixShow(A, 112, 1);
 }
 
